b52d7bde30adc8fe310cde6743fa9c882243c632
WaBuyange37 <nzelabeatblack37@gmail.com>
2025-03-06 15:16:26 +0200

    Moshy simple Dom-if ndebe

--- MoshNow/person.js
+++ MoshNow/person.js
@@ -3,6 +3,16 @@ let person={
   age: 26,
   hobbie: 'JavaScript',
 };
+let myList = [122,5,'nkunda js',person.name];
+myList[3]='ndebe';
+console.log(typeof myList);
+console.log(myList);
+if(person.name === myList[3]){
 document.getElementById('name').innerText=person.name;
 document.getElementById('age').innerText=person.age;
+}
+else{
+  document.getElementById('name').innerText='unknown';
+document.getElementById('age').innerText='unknown';
+}
 console.log(`my name is: ${person.name}\n I have ${person.age} Years old\n I love ${person.hobbie}`);
