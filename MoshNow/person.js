8a0a37c02c347430a915c51ad9322a11808a94b0
WaBuyange37 <nzelabeatblack37@gmail.com>
2025-03-06 14:52:55 +0200

    Moshy simple Dom

--- MoshNow/person.js
+++ MoshNow/person.js
@@ -3,4 +3,6 @@ let person={
   age: 26,
   hobbie: 'JavaScript',
 };
+document.getElementById('name').innerText=person.name;
+document.getElementById('age').innerText=person.age;
 console.log(`my name is: ${person.name}\n I have ${person.age} Years old\n I love ${person.hobbie}`);
