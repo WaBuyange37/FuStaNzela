7db684f18567f5e845fb8318783e95ac380be25b
WaBuyange37 <nzelabeatblack37@gmail.com>
2025-03-13 10:28:23 +0200

    naka Fizzbuzz updates

--- MoshNow/fizzbuzz.js
+++ MoshNow/fizzbuzz.js
@@ -3,12 +3,22 @@
 // divisible by both 3 and 5 we get Fizz
 // !divisible by 3 and 5 we get that number
 function fizzBuzz(number){
-  // if(number % 3 === 0){
-  //   re
-  // }
+  //  if(number % 3 === 0 && number % 5 === 0){
+  //   return "fizzbuzz"
+  //  }
+  //  else if(number % 3 ===0){
+  //   return "fizz"
+  //  }
+  //  else if(number % 5 ===0){
+  //   return "buzz"
+  //  }
+  //  else{
+  //   return number;
+  //  }
   return ((number % 5 === 0) && (number % 3 ===0)) ? "FizzBuzz"
           : (number % 3 === 0)? "Fizz"
           : (number % 5 === 0) ? "Buzz"
+          : (typeof number !=="number") ? "not a number"
           : number;
 }
 let k = fizzBuzz(15);
